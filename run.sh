5c7fdcde65248d63722e4a8ca540cbe2caed9cf5
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2024-07-06 22:31:31 +0200

    add orig bz2 search

--- run.sh
+++ run.sh
@@ -7,13 +7,18 @@ main() {
   ARCHIVE=/var/spool/apt-mirror/mirror/archive.ubuntu.com/ubuntu/pool
   REPOSITORY=main
 
+  RUNID=$(head -2 /dev/urandom | tr -cd '[:alnum:]' | head -c 4)
+  echo "RUNID=${RUNID}"
+
   for pkg in "${ARCHIVE}"/"${REPOSITORY}"/*/*
   do
-    echo "${pkg}"
+    echo -n "${pkg}: "
     if ls "${pkg}"/*.orig.tar.gz 1> /dev/null 2>&1; then
       echo "gz exists"
     elif ls "${pkg}"/*.orig.tar.xz 1> /dev/null 2>&1; then
       echo "xz exists"
+    elif ls "${pkg}"/*.orig.tar.bz2 1> /dev/null 2>&1; then
+      echo "bz2 exists"
     else
       echo "does not exists"
     fi   
