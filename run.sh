81f91856382065fc5b4501109a902668f5e61fcc
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2024-07-07 22:23:09 +0200

    detect language

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 cleanup() {
-  test -d "${tmp_dir}" && rm -fr "${tmp_dir}"
+  test -d "${tmp_dir}" && rm -rf "${tmp_dir}"
 }
 
-me=$(basename "$0")
-tmp_dir=$(mktemp -d -t "${me}".XXXX)
+self=$(basename "$0")
+tmp_dir=$(mktemp -d -t "${self}".XXXX)
 echo "${tmp_dir}"
 
-trap cleanup EXIT
+# trap cleanup EXIT
 
 main() {
 
@@ -19,20 +19,29 @@ main() {
 
   for pkg in "${ARCHIVE}"/"${REPOSITORY}"/*/*
   do
-    echo -n "${pkg}: "
     if ls "${pkg}"/*.orig.tar.gz 1> /dev/null 2>&1; then
-      echo "orig gz exists"
+      compressed_src=$(ls "${pkg}"/*.orig.tar.gz | sort | tail -n 1)
     elif ls "${pkg}"/*.orig.tar.xz 1> /dev/null 2>&1; then
-      echo "orig xz exists"
+      compressed_src=$(ls "${pkg}"/*.orig.tar.xz | sort | tail -n 1)
     elif ls "${pkg}"/*.orig.tar.bz2 1> /dev/null 2>&1; then
-      echo "orig bz2 exists"
+      compressed_src=$(ls "${pkg}"/*.orig.tar.bz2 | sort | tail -n 1)
     elif ls "${pkg}"/*.tar.xz 1> /dev/null 2>&1; then
-      echo "some xz exists"
+      compressed_src=$(ls "${pkg}"/*.tar.xz | sort | tail -n 1)
     elif ls "${pkg}"/*.tar.gz 1> /dev/null 2>&1; then
-      echo "some gz exists"
+      compressed_src=$(ls "${pkg}"/*.tar.gz | sort | tail -n 1)
     else
-      echo "does not exists"
-    fi   
+      echo "err: compressed src does not exist"
+    fi
+
+    pkg_name=$(basename "${pkg}")
+    tmp_pkg_loc="${tmp_dir}"/"${pkg_name}"
+    mkdir "${tmp_pkg_loc}"
+
+    echo -n "${pkg_name}: "
+
+    tar -xf "${compressed_src}" -C "${tmp_pkg_loc}" --strip-components 1
+    (cd "${tmp_pkg_loc}" && git init -q && git add . && git commit -q -m 'commit'&& github-linguist -j)
+
   done
 }
 
