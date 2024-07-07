8a2a1161b8a5fe695e8a52954f8eb918fa0d360f
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2024-07-07 20:21:00 +0200

    add exit trap

--- run.sh
+++ run.sh
@@ -2,23 +2,34 @@
 
 set -euo pipefail
 
+cleanup() {
+  test -d "${tmp_dir}" && rm -fr "${tmp_dir}"
+}
+
+me=$(basename "$0")
+tmp_dir=$(mktemp -d -t "${me}".XXXX)
+echo "${tmp_dir}"
+
+trap cleanup EXIT
+
 main() {
 
   ARCHIVE=/var/spool/apt-mirror/mirror/archive.ubuntu.com/ubuntu/pool
   REPOSITORY=main
 
-  RUNID=$(head -2 /dev/urandom | tr -cd '[:alnum:]' | head -c 4)
-  echo "RUNID=${RUNID}"
-
   for pkg in "${ARCHIVE}"/"${REPOSITORY}"/*/*
   do
     echo -n "${pkg}: "
     if ls "${pkg}"/*.orig.tar.gz 1> /dev/null 2>&1; then
-      echo "gz exists"
+      echo "orig gz exists"
     elif ls "${pkg}"/*.orig.tar.xz 1> /dev/null 2>&1; then
-      echo "xz exists"
+      echo "orig xz exists"
     elif ls "${pkg}"/*.orig.tar.bz2 1> /dev/null 2>&1; then
-      echo "bz2 exists"
+      echo "orig bz2 exists"
+    elif ls "${pkg}"/*.tar.xz 1> /dev/null 2>&1; then
+      echo "some xz exists"
+    elif ls "${pkg}"/*.tar.gz 1> /dev/null 2>&1; then
+      echo "some gz exists"
     else
       echo "does not exists"
     fi   
