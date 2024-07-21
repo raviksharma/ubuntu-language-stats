fe9878722e3d4d65b3d21184b03c06bdd9f40d5e
Ravi Kant Sharma <600723+raviksharma@users.noreply.github.com>
2024-07-21 21:28:07 +0200

    add chart css wip 1

--- transform.py
+++ transform.py
@@ -8,30 +8,27 @@ def main() -> None:
     f = open(sys.argv[1], 'r')
     _ = f.readline()
 
-    #out = open('bars.html', 'w')
-    print('<!DOCTYPE html>\n<html>\n<head>\n<link rel="stylesheet" href="chart.css">\n</head>\n<body>\n<div class="chart">\n')
-    #print('name,'+','.join(recognised_langs))
+    print('<!DOCTYPE html>\n<html>\n<head>\n\t<link rel="stylesheet" href="chart.css">\n</head>\n<body>\n<div class="chart">')
     try:
         while line := f.readline():
             pkg, languages_s = line.split(':', 1)
             languages = json.loads(languages_s)
             other = 0.0
-            print(f'<div class="bar">\n<div class="label">{pkg}</div>\n')
-            for lang, v in languages.items():
+            print(f'\t<div class="bar">\n\t\t<div class="label">{pkg}</div>')
+            for lang, v in dict(sorted(languages.items(), key=lambda item: float(item[1]['percentage']), reverse=True)).items():
                 p = v['percentage']
                 if float(p) >= 5.0:
-                    print(f'<div class="section" style="--section-value: {p};" data-value="{lang} ({p})"></div>\n')
-                    #print(pkg+','+lang+','+p)
+                    print(f'\t\t<div class="section" style="--section-value: {p};" data-value="{lang} ({p})"></div>')
                 else:
                     other += float(p)
             if other > 0.0:
-                print(f'<div class="section" style="--section-value: {p};" data-value="other ({other})"></div>\n')
-            #print(pkg+',other,'+"{:.2f}".format(other))
-            print('</div>\n')
+                print(f'\t\t<div class="section" style="--section-value: {other:.2f};" data-value="other ({other:.2f})"></div>')
+            print('\t</div>')
     except Exception as e:
         ipdb.set_trace()
-    #out.write('</div>\n</body>\n</html>')
-    #out.close()
+    print('</div>')
+    print('</body>')
+    print('</html>')
     return 0
 
 
